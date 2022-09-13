145e2292483e618f331e603ab302e8d290335e70
Dell <Dell@AGNatewad-100>
2022-09-13 10:10:49 +0530

    Assignments_2

---
+++ Assignment/src/com/forloopassignment/PalindromeRange.java
@@ -0,0 +1,33 @@
+package com.forloopassignment;
+
+
+
+public class PalindromeRange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int num,sum=0,i,temp;
+		
+		for(num=100;num<=500;num++)
+		{
+			for(i=num;i>0;i=i/10)
+			{
+			temp=i%10;
+			sum=sum*10+temp;
+			}
+		}
+		if(sum==num)
+		{
+			System.out.println(num+" is palindrome");
+		}
+		else
+		{
+			System.out.println(num+" is not palindrome");
+		}
+
+
+	}
+
+}
---
+++ Assignment/src/exception/com/TestThrowPractice.java
@@ -0,0 +1,24 @@
+package exception.com;
+
+public class TestThrowPractice {
+
+	static void check() throws ArithmeticException {  
+
+		System.out.println("Inside check function");
+
+		throw new ArithmeticException("demo");
+	}
+
+	public static void main(String [] args) {
+		// TODO Auto-generated method stub
+
+		try
+		{
+			check();
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("caught" + e);
+		}
+	}
+}
